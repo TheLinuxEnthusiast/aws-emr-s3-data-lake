675fcafbbe7b66a0653a46f0e50fbb2fba9d7282
darren.foley <darren.foley@ucdconnect.ie>
2021-09-08 20:45:47 +0000

    Added template config file, updated etl.py script

--- etl.py
+++ etl.py
@@ -1,6 +1,6 @@
+import os
 import configparser
 from datetime import datetime
-import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
@@ -9,53 +9,91 @@ from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dat
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+#os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+#os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY=config.get('AWS', 'AWS_ACCESS_KEY_ID')
+AWS_SECRET_KEY=config.get('AWS', 'AWS_SECRET_ACCESS_KEY')
+S3_DESTINATION=config.get('S3', 'S3_DESTINATION')
+S3_INPUT=config.get('S3', 'S3_INPUT')
 
 
+
+"""
 def create_spark_session():
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
         .getOrCreate()
     return spark
+"""
+
+def create_spark_session():
+    """
+    Description: Function that manages the creation of a spark session to the local cluster.
+    
+    Arguments:
+        None
+        
+    Returns: 
+        sqlContext - Spark Sql Context Object
+    """
+    conf = SparkConf()
+    conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.0')
+    sc = SparkContext(conf=conf)
+
+    # add aws credentials
+    sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", AWS_ACCESS_KEY)
+    sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", AWS_SECRET_KEY)
+    sc._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
+    #creating the context
+    sqlContext = SQLContext(sc)
+    
+    return sqlContext
+
 
 
 def process_song_data(spark, input_data, output_data):
-    # get filepath to song data file
-    song_data = 
+    """
+    Description: Processes song and artist data and writes a parquet file to S3
     
-    # read song data file
-    df = 
+    Arguments:
+        spark - spark SQL context obect
+        input_data - path to S3 input bucket
+        output_data - path to S3 output bucket
+    
+    Returns:
+        None
+    """
+    
+    song_data = input_data + "song_data/"
+    
+    df = spark.read.json(song_data)
 
-    # extract columns to create songs table
-    songs_table = 
-    
-    # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table = df.select(["song_id","title","artist_id","year","duration"]).distinct()
 
-    # extract columns to create artists table
-    artists_table = 
-    
-    # write artists table to parquet files
-    artists_table
+    songs_table.write.format("parquet").partitionBy("year", "artist_id").mode("overwrite").save(output_data + "dim_song/")
+
+    artists_table = song_data.select(["artist_id","artist_name","artist_location","artist_latitude","artist_longitude"]).distinct()
+
+    artist_table.write.format("parquet").partitionBy("artist_name").mode("overwrite").save(output_data + "dim_artist/")
 
 
 def process_log_data(spark, input_data, output_data):
     # get filepath to log data file
-    log_data =
+    log_data = input_data + "log_data/"
 
     # read log data file
-    df = 
+    df = spark.read.json(log_data)
     
     # filter by actions for song plays
     df = 
 
     # extract columns for users table    
-    artists_table = 
+    users_table = 
     
     # write users table to parquet files
-    artists_table
+    users_table
 
     # create timestamp column from original timestamp column
     get_timestamp = udf()
@@ -83,11 +121,9 @@ def process_log_data(spark, input_data, output_data):
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = "s3a://sparkify-data-lake-df/"
     
-    process_song_data(spark, input_data, output_data)    
-    process_log_data(spark, input_data, output_data)
+    process_song_data(spark, S3_INPUT, S3_DESTINATION)    
+    process_log_data(spark, S3_INPUT, S3_DESTINATION)
 
 
 if __name__ == "__main__":
