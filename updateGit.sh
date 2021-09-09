2d0ab3fb97412725b2cffaf225298012e3ce36c6
darren.foley <darren.foley@ucdconnect.ie>
2021-09-09 18:51:23 +0000

    added git workflow

--- updateGit.sh
+++ updateGit.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+apt-get install -y openssh-client
+
 git config --global user.name "darren.foley"
 git config --global user.email "darren.foley@ucdconnect.ie"
+
+mkdir -p $HOME/.ssh
+
+ssh-keygen -t rsa
+
+git remote add origin git@github.com:AnalyticsEnthusiast/aws-emr-s3-data-lake.git
